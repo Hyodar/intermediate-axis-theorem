adf88b0392b06e00fc67e4ed78f8b965eb7d09a8
Franco Barpp Gomes <franco.b.gomes@gmail.com>
2019-07-29 14:27:07 -0300

    Simple corrections

--- src/utils/classes/Tbar.py
+++ src/utils/classes/Tbar.py
@@ -49,23 +49,23 @@ class Tbar:
     def _compute_moment_inertia(self):
 
         # distance between center of mass of handle and of the total object
-        self.cm_distance = - (self.axis.length / 6 + self.handle.radius / 3)
+        self.cm_distance = - (self.axis.height / 6 + self.handle.radius / 3)
 
         ixx = ((1 / 2) * (self.handle.mass) * (self.handle.radius ** 2)
                + (self.handle.mass) * self.cm_distance ** 2 + (self.axis.mass / 4) * (self.axis.radius ** 2)
-               + (self.axis.mass / 12) * (self.axis.length ** 2)
-               + self.axis.mass * (self.axis.length / 2 + self.handle.radius - self.cm_distance))
+               + (self.axis.mass / 12) * (self.axis.height ** 2)
+               + self.axis.mass * (self.axis.height / 2 + self.handle.radius - self.cm_distance))
 
         iyy = ((1 / 4) * (self.handle.mass) * (self.handle.radius ** 2)
-               + (1 / 12) * (self.handle.mass) * (self.handle.length ** 2)
+               + (1 / 12) * (self.handle.mass) * (self.handle.height ** 2)
                + self.handle.mass * (self.cm_distance ** 2)
                + (1 / 4) * self.axis.mass * (self.axis.radius ** 2)
-               + (1 / 12) * (self.axis.mass) * (self.axis.length ** 2)
-               + self.axis.mass * (self.axis.length / 2 + self.handle.radius - self.cm_distance))
+               + (1 / 12) * (self.axis.mass) * (self.axis.height ** 2)
+               + self.axis.mass * (self.axis.height / 2 + self.handle.radius - self.cm_distance))
 
         izz = ((1 / 2) * (self.axis.mass) * (self.axis.radius ** 2)
                + (1 / 4) * (self.handle.mass) * (self.handle.radius ** 2)
-               + (1 / 12) * (self.handle.mass) * (self.handle.length ** 2))
+               + (1 / 12) * (self.handle.mass) * (self.handle.height ** 2))
 
         self.moment_inertia = np.array((ixx, iyy, izz))
 
